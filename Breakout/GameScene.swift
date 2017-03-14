44cb2984380265c87a9ea10b600ad8ba3f2a5100
ckerr <student@Ckerr.local>
2017-03-14 11:47:10 -0500

    Determine winners and losers

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -41,6 +41,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             paddle.position.x = location.x
         }
     }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick"{
+            print("You win!")
+            brick.removeFromParent()
+            ball.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "loseZone" || contact.bodyB.node?.name == "loseZone"{
+            print("You lose!")
+            ball.removeFromParent()
+        }
+    }
    
     func createBackground(){
         let stars = SKTexture(imageNamed: "stars")
@@ -108,12 +120,4 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         loseZone.physicsBody?.isDynamic = false
         addChild(loseZone)
     }
-    
-    
-    
-    
-    
-    
-    
- 
    }
